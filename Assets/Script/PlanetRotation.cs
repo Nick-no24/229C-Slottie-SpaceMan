fb8019acefaf011870c261f76e96f26a5614746f
Nick-no24 <nobphadon.chan@bumail.net>
2025-03-24 22:16:30 +0700

    Mars Map Update

---
+++ Assets/Script/PlanetRotation.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+
+
+public class PlanetRotation : MonoBehaviour
+{
+    public float rotationSpeed = 10f; 
+
+    void Update()
+    {
+       // transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
